a4a16566fbcf64ddf37a94f997871ee994c5d34d
Andrea <a2020122545@estudiantes.upsa.edu.bo>
2021-11-05 22:56:38 -0400

    CatalogosController creado

---
+++ CatalogoDeTienda/Controllers/CatalogosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CatalogoDeTienda.Data;
+using CatalogoDeTienda.Models;
+
+namespace CatalogoDeTienda.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CatalogosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Catalogos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Catalogo>>> GetCatalogo()
+        {
+            return await _context.Catalogo.ToListAsync();
+        }
+
+        // GET: api/Catalogos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Catalogo>> GetCatalogo(int id)
+        {
+            var catalogo = await _context.Catalogo.FindAsync(id);
+
+            if (catalogo == null)
+            {
+                return NotFound();
+            }
+
+            return catalogo;
+        }
+
+        // PUT: api/Catalogos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCatalogo(int id, Catalogo catalogo)
+        {
+            if (id != catalogo.RopaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(catalogo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CatalogoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Catalogos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Catalogo>> PostCatalogo(Catalogo catalogo)
+        {
+            _context.Catalogo.Add(catalogo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCatalogo", new { id = catalogo.RopaId }, catalogo);
+        }
+
+        // DELETE: api/Catalogos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCatalogo(int id)
+        {
+            var catalogo = await _context.Catalogo.FindAsync(id);
+            if (catalogo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Catalogo.Remove(catalogo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CatalogoExists(int id)
+        {
+            return _context.Catalogo.Any(e => e.RopaId == id);
+        }
+    }
+}
