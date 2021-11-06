3d94dd1688c609343964eeaf926a12054964658e
Andrea <a2020122545@estudiantes.upsa.edu.bo>
2021-11-05 22:54:54 -0400

    DbContext agregado al archivo Startup

--- CatalogoDeTienda/Startup.cs
+++ CatalogoDeTienda/Startup.cs
@@ -1,7 +1,9 @@
+using CatalogoDeTienda.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,10 @@ namespace CatalogoDeTienda
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogoDeTienda", Version = "v1" });
             });
+
+            services.AddDbContext<AppDbContext>(options =>
+             options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
